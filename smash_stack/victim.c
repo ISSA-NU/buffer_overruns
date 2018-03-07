b231cd9e0e44e8251199df8110f5faeebfdd2282
nugmeg <liu.ron@husky.neu.edu>
2018-03-07 13:18:13 -0500

    add problems

--- smash_stack/victim.c
+++ smash_stack/victim.c
@@ -2,16 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 
-void foo(const char *buff1) {
+void bar() { print("Are you watching closely?\n"); }
+
+int foo(const char *buff1) {
   unsigned int a = 0xbeef;
   char buff[256];
   strcpy(buff, buff1);
   if (a == 0xfeed) {
-    printf("Part III completed\n");
+    printf("Feed me with buffer.\n");
   }
+  return 1;
 }
 
 int main(int argc, char *argv[]) {
-  foo(argv[1]);
+  int a = foo(argv[1]);
   return 0;
 }
